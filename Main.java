0020044e99ce3e470b1bdacd3ebd38de6fdf417a
Lin Liu <lin_liu@listech.on.ca>
2022-02-25 15:23:18 -0500

    update

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main{
+    public static void main(String[] args){
+        //Tree (root)
+        //nodes(value,left child,right child)
+        //insert(value)
+        //find(value):boolean
+        Tree tree=new Tree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(10);
+        System.out.println("done");
+    }
+}
