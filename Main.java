6483c91b88db99ea20ab9b5d45de54c7204b7424
Lin Liu <liulinradio@gmail.com>
2022-02-26 20:55:48 -0500

    update

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
-class Main{
-    public static void main(String[] args){
-        //Tree (root)
-        //nodes(value,left child,right child)
-        //insert(value)
-        //find(value):boolean
-        Tree tree=new Tree();
+class Main {
+    public static void main(String[] args) {
+        // Tree (root)
+        // nodes(value,left child,right child)
+        // insert(value)
+        // find(value):boolean
+        Tree tree = new Tree();
         tree.insert(7);
         tree.insert(4);
         tree.insert(9);
         tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
         tree.insert(10);
-        System.out.println(tree.find(11));
+        // tree.traversePreOrder();
+        // tree.traverseInOrder();
+        tree.traversePostOrder();
     }
+
 }
--- Tree.java
+++ Tree.java
@@ -1,64 +1,103 @@
 public class Tree {
-    public class Node{
+    public class Node {
         private int value;
         private Node leftChild;
         private Node rightChild;
 
-        public Node(int value){
-            this.value=value;
+        public Node(int value) {
+            this.value = value;
         }
+
         @Override
-        public String toString(){
-            return "Node="+value;
+        public String toString() {
+            return "Node=" + value;
         }
     }
 
     private Node root;
-    public void insert(int value){
-        Node node=new Node(value);
-        if(root==null){
-            root=node;
+
+    public void insert(int value) {
+        Node node = new Node(value);
+        if (root == null) {
+            root = node;
             return;
         }
-        Node current=root;
-        while(true){
-            if(value<current.value){
-                if(current.leftChild==null){
-                    //find the parent
-                    current.leftChild=node;
+        Node current = root;
+        while (true) {
+            if (value < current.value) {
+                if (current.leftChild == null) {
+                    // find the parent
+                    current.leftChild = node;
                     break;
                 }
-                current=current.leftChild;
-            }
-            else{
-                if(current.rightChild==null){
-                    current.rightChild=node;
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
                     break;
                 }
-                current=current.rightChild;
+                current = current.rightChild;
             }
-                
+
         }
     }
-    public boolean find(int value){
-         Node current=root;
-         while(current!=null){
-             if(value<current.value)
-             {
-                 current=current.leftChild;
-             }
-             else if(value>current.value){
-                 current=current.rightChild;
-             }
-             else{
-                 return true;
-             }
-         }
-         return false;
+
+    public boolean find(int value) {
+        Node current = root;
+        while (current != null) {
+            if (value < current.value) {
+                current = current.leftChild;
+            } else if (value > current.value) {
+                current = current.rightChild;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 
-    //PRE-ORDER TRAVERSAL: ROOT LEFT RIGHT 
-    //IN-ORDRE:  LEFT ROOT RIGHT  ASCENDING ORDER 
-    //           RIGHT ROOT LEFT  DESCENDING ORDER
-    //POST-ORDER:LEFT RIGHT ROOT
+    // PRE-ORDER TRAVERSAL: ROOT LEFT RIGHT
+    // IN-ORDRE: LEFT ROOT RIGHT ASCENDING ORDER
+    // RIGHT ROOT LEFT DESCENDING ORDER
+    // POST-ORDER:LEFT RIGHT ROOT
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    private void traversePreOrder(Node root) {
+        if (root == null)
+            return;
+        System.out.println(root.value);
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+        // root print
+        // left children
+        // right children
+    }
+
+    public void traverseInOrder() {
+        traverseInOrder(root);
+    }
+
+    private void traverseInOrder(Node root) {
+        if (root == null)
+            return;
+        traverseInOrder(root.leftChild);
+        System.out.println(root.value);
+        traverseInOrder(root.rightChild);
+    }
+
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    private void traversePostOrder(Node root) {
+        if (root == null)
+            return;
+
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.println(root.value);
+    }
+
 }
